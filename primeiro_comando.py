a9b73fe4f0b57cfd935b4a19b62b144614a6130f
OwayranTorquato <owayran1998@gmail.com>
2023-08-04 22:06:04 -0300

    alteração

--- primeiro_comando.py
+++ primeiro_comando.py
@@ -1,3 +1,8 @@
 print("Olá mundo")
 print("Teste python")
 print("ok")
+import yfinance as yf
+import pandas as pd
+ticker = ("PETR4.SA","BBDC4.SA")
+data = yf.download(ticker, start="2022-01-01", end="2023-01-01")
+print (data)
