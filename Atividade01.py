009b5b45f3002aacca3bf3207186175f8aa82d45
OwayranTorquato <owayran1998@gmail.com>
2023-08-09 21:55:21 -0300

    Update

---
+++ Atividade01.py
@@ -0,0 +1,14 @@
+nome = input("Owayran")
+sobrenome = "Torquato Fernandes da Silva"
+idade = 24
+altura = 1.84
+peso = "93kg"
+maior_idade = "Sim"
+
+print (nome)
+print (sobrenome)
+print (idade)
+print (altura)
+print (peso)
+print(maior_idade)
+
