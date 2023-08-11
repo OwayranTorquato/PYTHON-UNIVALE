c389b5b03a80a1733100ff9a5860a2a18758af80
OwayranTorquato <owayran1998@gmail.com>
2023-08-11 20:21:19 -0300

    Update

--- Atividade04_calculadora.py
+++ Atividade04_calculadora.py
@@ -1,13 +1,10 @@
 #importando as funções a serem utilizadas
 def adicao (x,y):
     return x+y
-
 def subtracao (x,y):
     return x-y
-
 def multiplicacao (x,y):
     return x*y
-
 def divisao (x,y):
     if y !=0:
         return x/y
@@ -22,11 +19,10 @@ print ("4: DIVISÃO")
 
 #espaço reservado para entrada de dados do usuário
 escolha = input("Digite a o número da operação a ser realizada: ")
-
 num1 = float(input("Digite um número: "))
 num2 = float(input("Digite um número: "))
 
-#selecionando as condicionais
+#selecionando as condicionais para mostrar o resultado na tela
 if escolha == '1':
     print (num1, "+",num2,"=",adicao(num1,num2))
 elif escolha=='2':
@@ -37,4 +33,3 @@ elif escolha=='4':
     print (num1,"/",num2,"=",divisao(num1,num2))
 else:
     print ("Operação inválida")
-
---
+++ interface_grafica_calculadora.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Funções de operações matemáticas
+def adicao(x, y):
+    return x + y
+
+def subtracao(x, y):
+    return x - y
+
+def multiplicacao(x, y):
+    return x * y
+
+def divisao(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Não é possível realizar a divisão por 0"
+
+# Função para calcular a operação selecionada
+def calcular():
+    escolha = operacao.get()
+    num1 = float(entry_num1.get())
+    num2 = float(entry_num2.get())
+
+    if escolha == 1:
+        resultado.set(adicao(num1, num2))
+    elif escolha == 2:
+        resultado.set(subtracao(num1, num2))
+    elif escolha == 3:
+        resultado.set(multiplicacao(num1, num2))
+    elif escolha == 4:
+        resultado.set(divisao(num1, num2))
+    else:
+        messagebox.showerror("Erro", "Operação inválida")
+
+# Configuração da interface gráfica
+root = tk.Tk()
+root.title("Calculadora")
+
+operacao = tk.IntVar()
+
+label_operacao = tk.Label(root, text="Escolha a operação:")
+label_operacao.pack()
+
+radio_adicao = tk.Radiobutton(root, text="Adição", variable=operacao, value=1)
+radio_adicao.pack()
+
+radio_subtracao = tk.Radiobutton(root, text="Subtração", variable=operacao, value=2)
+radio_subtracao.pack()
+
+radio_multiplicacao = tk.Radiobutton(root, text="Multiplicação", variable=operacao, value=3)
+radio_multiplicacao.pack()
+
+radio_divisao = tk.Radiobutton(root, text="Divisão", variable=operacao, value=4)
+radio_divisao.pack()
+
+label_num1 = tk.Label(root, text="Digite o primeiro número:")
+label_num1.pack()
+
+entry_num1 = tk.Entry(root)
+entry_num1.pack()
+
+label_num2 = tk.Label(root, text="Digite o segundo número:")
+label_num2.pack()
+
+entry_num2 = tk.Entry(root)
+entry_num2.pack()
+
+calcular_button = tk.Button(root, text="Calcular", command=calcular)
+calcular_button.pack()
+
+resultado = tk.StringVar()
+label_resultado = tk.Label(root, textvariable=resultado)
+label_resultado.pack()
+
+root.mainloop()
