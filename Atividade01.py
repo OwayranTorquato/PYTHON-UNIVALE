80c6e6c4c6e9af2c35df3027395ec7f18b2af072
OwayranTorquato <owayran1998@gmail.com>
2023-08-10 21:59:26 -0300

    Update

--- Atividade01.py
+++ Atividade01.py
@@ -1,14 +1,17 @@
-nome = input("Owayran")
+nome = "Owayran"
 sobrenome = "Torquato Fernandes da Silva"
 idade = 24
 altura = 1.84
 peso = "93kg"
 maior_idade = "Sim"
-
+number1 = input()
+number2 =input()
 print (nome)
 print (sobrenome)
 print (idade)
 print (altura)
 print (peso)
 print(maior_idade)
+valor = int(number1)*int(number2)
+print(valor)
 
---
+++ calculo_imc.py
@@ -0,0 +1,6 @@
+peso = float(input("Informe seu peso: "))
+altura = float(input("informe a sua altura: "))
+
+IMC = peso /(altura**2)
+
+print(IMC)
