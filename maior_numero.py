d1f7e2238529a6f5ba93bfad9892345f43320368
OwayranTorquato <owayran1998@gmail.com>
2023-08-12 02:30:46 -0300

    Update

---
+++ maior_numero.py
@@ -0,0 +1,11 @@
+#definindo as váriaveis a receber os números
+numero1 = int(input("Insira um número: "))
+numero2 = int(input("Insira o próximo número: "))
+
+if (numero1>numero2):
+    maior_numero = (numero1)
+else: 
+    numero2>numero1
+maior_numero=(numero2)
+
+print(maior_numero)
