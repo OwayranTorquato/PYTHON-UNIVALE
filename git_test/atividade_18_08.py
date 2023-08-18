d08f5fe56c1a64c8b13d2c31a1cb020557ed99d1
OwayranTorquato <owayran1998@gmail.com>
2023-08-18 20:19:08 -0300

    Update

---
+++ git_test/atividade_18_08.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+
+
+def inseriridade():
+     idade=int(entry_idade.get())
+     if idade >= 18:
+          resultado.set('Você é maior de idade!')
+     else:
+          resultado.set('Você é menor de idade!')
+    
+
+root = tk.Tk()
+root.title("Calculadora")
+
+label_idade = tk.Label(root,text="Digite a sua idade: ")
+label_idade.pack()
+
+entry_idade=tk.Entry(root)
+entry_idade.pack()
+
+verificar = tk.Button(root, text="Verificar", command=inseriridade)
+verificar.pack()
+
+resultado=tk.StringVar()
+label_resultado = tk.Label(root,textvariable=resultado)
+label_resultado.pack()
+
+
+root.mainloop()
